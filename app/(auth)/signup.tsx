d33e8fcbb47930991f6d5dbeb1fb6e351142c792
dilusha-ola <dilushaolaboduwa3@gmail.com>
2025-11-25 09:10:58 +0530

    add password veiw icon

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -4,15 +4,14 @@ import { useAuth } from '@/context/auth-context';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
 import {
-  Alert,
-  KeyboardAvoidingView,
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    KeyboardAvoidingView,
+    Platform,
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
 } from 'react-native';
 
 export default function SignUpScreen() {
